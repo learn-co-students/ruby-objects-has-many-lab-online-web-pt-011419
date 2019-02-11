f93d69c90afc52989d4f95e0343b7be7c88b0ebc
Max Brooks <maxbrooks114@gmail.com>
2019-02-11 22:39:51 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+  attr_accessor :artist, :name, :genre
+ 
+ 
+ @@all=[]
+  
+  def initialize(name)
+    @name = name
+    @@all<< self 
+   end 
+  def self.all 
+    @@all
+  end
+  
+  def artist_name
+    if self.artist != nil
+      self.artist.name
+    else 
+      nil
+    end
+  end
+  
+  
+end
