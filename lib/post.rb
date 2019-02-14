7a161b7f09b7a6d572cf1496cf9b8e84bb858463
Dylan Michael Labat <dml252@live.com>
2019-02-14 02:09:30 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    @author = author
+    @@all << self
+  end
+  
+  def author_name
+    self.author ? self.author.name : nil
+  end
+  
+  def self.all
+    @@all
+  end
+end
